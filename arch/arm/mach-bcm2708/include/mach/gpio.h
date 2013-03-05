5bfee162f1380bae54bd8a6060f64ec8ca01add5
Lubomir Rintel <lkundrak@v3.sk>
2013-03-05 11:48:11 +0100

    RPi: mach-bcm2708: Broadcom BCM2708 Development Platform

---
+++ arch/arm/mach-bcm2708/include/mach/gpio.h
@@ -0,0 +1,26 @@
+/*
+ * arch/arm/mach-bcm2708/include/mach/gpio.h
+ *
+ * This file is licensed under the terms of the GNU General Public
+ * License version 2.  This program is licensed "as is" without any
+ * warranty of any kind, whether express or implied.
+ */
+
+#ifndef __ASM_ARCH_GPIO_H
+#define __ASM_ARCH_GPIO_H
+
+#define BCM_NR_GPIOS 54 // number of gpio lines
+
+#include <asm-generic/gpio.h>
+#include <mach/platform.h>
+#include <mach/irqs.h>
+
+#ifdef CONFIG_GPIOLIB
+
+static inline unsigned irq_to_gpio(unsigned irq) {
+	return (irq-GPIO_IRQ_START);
+}
+
+#endif /* CONFIG_GPIOLIB */
+
+#endif
